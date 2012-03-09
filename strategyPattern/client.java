57952604022dee2f3ed934b4b6fca76014e90892
Willa <mowmowjoker@gmail.com>
2012-03-09 12:28:08 -0500

    iDelte

--- strategyPattern/client.java
+++
@@ -1,8 +0,0 @@
-public class client{
-public static void main(String[] args){
-int[] a = {4,5,6,7,23,19,25};
-sortContext s = new sortContext();
-s.setSorter(new Quicksort());
-s.sortInt(a);
-}
-}
--- strategyPattern/sort.java
+++
@@ -1,4 +0,0 @@
-public interface sort{
-
-public void sort(int[] list);
-}
