a1c2ba55b8f329b5c6db401a5bf8170b95843447
Willa Wang <willa@greenhouse.io>
2018-08-14 22:32:09 -0400

    update format

--- Sorting/mergeSort.java
+++ Sorting/mergeSort.java
@@ -1,71 +1,74 @@
 import java.util.Random;
 
-public class MergeSort{
+public class mergeSort{
 
 private int[] input;
 private static Random generator = new Random();
 
 public mergeSort(int size){
-	input = new int[size];
+  input = new int[size];
  for ( int i = 0; i < size ; i++ ){
-         input[i] = 10 + generator.nextInt(90);
-	System.out.println(Integer.toString(input[i]));
+  input[i] = 10 + generator.nextInt(90);
+  System.out.print(Integer.toString(input[i]) + " ");
 }
 }
 public mergeSort(int[] a){
-	this.input = a;
+  this.input = a;
 }
 
 public void sort(){
-	sortArray(0, input.length-1);
+  sortArray(0, input.length-1);
 }
 
 public void sortArray(int low, int high){
-	if ((high - low) >= 1) {
-		int middle1 = (high + low)/2;
-		System.out.println(input[middle1]);
-		int middle2 = middle1 + 1;
-		System.out.println("split" + subarray(low, high));
-		System.out.println("     " + subarray(low, middle1));
-		System.out.println("     " + subarray(middle2, high));
-		sortArray(low, middle1);
-		sortArray(middle2, high);
-		merge(low, middle1, middle2, high);
+  if ((high - low) >= 1) {
+    int middle1 = (high + low)/2;
+    int middle2 = middle1 + 1;
+    System.out.println("split" + subarray(low, high));
+    System.out.println("     " + subarray(low, middle1));
+    System.out.println("     " + subarray(middle2, high));
+    sortArray(low, middle1);
+    sortArray(middle2, high);
+    merge(low, middle1, middle2, high);
 }
 }
 
 public void merge(int low, int middle1, int middle2, int high){
 
-	int leftIndex = low;
-	int rightIndex = middle2;
-	int combineIndex = low;
-	int[] combine = new int[input.length];
-	System.out.println( "merge:   " + subarray( low, middle1 ) );
-        System.out.println( "         " + subarray( middle2,high ) );
-	
-	while(leftIndex <=middle1 && rightIndex <= high){
-		if (input[leftIndex] <= input[rightIndex]){
-			combine[combineIndex++] = input[leftIndex++];
+  int leftIndex = low;
+  int rightIndex = middle2;
+  int combineIndex = low;
+  int[] combine = new int[input.length];
+  System.out.println( "merge:   " + subarray( low, middle1 ) );
+  System.out.println( "         " + subarray( middle2,high ) );
+  System.out.println( "low " + Integer.toString(low) );
+  System.out.println( "high " + Integer.toString(high) );
+  System.out.println( "middle1 " + Integer.toString(middle1) );
+  System.out.println( "middle2 " + Integer.toString(middle2) );
+  
+  while(leftIndex <= middle1 && rightIndex <= high){
+    if (input[leftIndex] <= input[rightIndex]){
+      combine[combineIndex++] = input[leftIndex++];
 }
-		else{
-			combine[combineIndex++] = input[rightIndex++];
+    else{
+      combine[combineIndex++] = input[rightIndex++];
 }
 }
-	if (leftIndex == middle2){
-		while(rightIndex <= high){
-			combine[combineIndex++] = input[rightIndex++];
+  if (leftIndex == middle2){
+    while(rightIndex <= high){
+      combine[combineIndex++] = input[rightIndex++];
 }
 }
-	else{
-		while(leftIndex <= middle1){
-			combine[combineIndex++]= input[leftIndex++];
+  else{
+    while(leftIndex <= middle1){
+      combine[combineIndex++]= input[leftIndex++];
 }
 }
 
-	for (int i = low; i<=high;i++){
-		input[i] = combine[i];
-	System.out.println( "         " + subarray( low, high ) );
-      System.out.println();
+  for (int i = low; i<=high;i++){
+    input[i] = combine[i];
+  System.out.println( "         " + subarray( low, high ) );
+  System.out.println();
 }
 
 
@@ -93,8 +96,9 @@ public void merge(int low, int middle1, int middle2, int high){
    } // end method toString 
 
 public static void main(String[] args){
-	mergeSort m = new mergeSort(8);
-	m.sort();
+  int[] A = { 1, 2, 3, 4, 5, 6, 7};
+  mergeSort m = new mergeSort(A);
+  m.sort();
 
 }
 }
